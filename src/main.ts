723628b6a577b027043589b1f394b6cd5aa3e067
Hakim Maaouia <hakimmaaouia@gmail.com>
2021-04-19 17:11:47 +0100

    config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as config from 'config'
+import { Console } from 'node:console';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configserver = config.get('server')
+  await app.listen(process.env.PORT||configserver.prot);
+
 }
 bootstrap();
